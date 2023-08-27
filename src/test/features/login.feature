28735421933a6516de08d2fc813f59a1273a990e
sagar <sanutiwari7@gmail.com>
2023-08-27 14:59:37 +0800

    first commit

---
+++ src/test/features/login.feature
@@ -0,0 +1,6 @@
+Feature: User Authentication Test
+
+Scenario: Login valid User
+Given user launch url
+When search for product name
+Then user validate dashboard
---
+++ src/test/steps/loginsteps.ts
@@ -0,0 +1,37 @@
+import { Given,When,Then } from "@cucumber/cucumber";
+import { chromium, Page, Browser } from "@playwright/test";
+
+let browser:Browser;
+let page: Page;
+
+Given('user launch url',{timeout: 30000}, async function () {
+    // Write code here that turns the phrase above into concrete actions
+    browser = await chromium.launch({ headless:false});
+    page = await browser.newPage();
+    
+    await page.goto("https://www.flipkart.com/");
+
+    
+    // await page.setViewportSize({
+    //      width: 1640,
+    //      height: 597,
+    //    });
+
+    
+  });
+
+  When('search for product name',{timeout: 30000}, async function () {
+    // Write code here that turns the phrawait page.waitForTimeout(20000);
+
+    await page.locator("button[class ='_2KpZ6l _2doB4z']").click();
+    await page.locator("img[alt='Mobiles']").click();
+    
+  });
+
+
+  Then('user validate dashboard', async function () {
+    
+    // Write code here that turns the phrase above into concrete actions
+    await browser.close();
+
+  });
